aab9aa916ddfff33c65ef6629179ba6b8652b614
xtepwxly <briareibismuth@gmail.com>
2025-03-07 21:56:12 +0300

    error problems

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -42,7 +42,10 @@ export async function fetchWithToken(url: string, config = { headers: {}}) {
     const resp = await axios(url, finalConfig);
     return resp;
   } catch (error) {
-    if (error instanceof AxiosError && error.code === 'ERR_BAD_REQUEST' && (error.status === 400 || error.status === 500)) {
+    if (error instanceof AxiosError && (
+        (error.code === 'ERR_BAD_REQUEST' && error.status === 400) || 
+        (error.code === 'ERR_BAD_RESPONSE' && error.status === 500))
+      ) {
       console.log('error', error.toJSON());
       throw error;
     } else {
