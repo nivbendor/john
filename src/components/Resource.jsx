f9776ea0b4906c7a58deb8bb23bd86414950f723
xtepwxly <briareibismuth@gmail.com>
2025-04-04 16:36:45 +0300

    update booklet url

--- src/components/Resource.jsx
+++ src/components/Resource.jsx
@@ -27,7 +27,7 @@ export const insuranceResources = [
   { name: 'Dental', pdfUrl: 'https://drive.google.com/file/d/104CY3yNGQO7CndksnwDaCKQ7NATsm5pq/view?usp=sharing' },
   { name: 'Critical Illness/Cancer', pdfUrl: 'https://drive.google.com/file/d/16ErPUOdmifHNue6XNfZ_tbR0LRRTcaDc/view?usp=sharing' },
   { name: 'Hospital Indemnity', pdfUrl: 'https://drive.google.com/file/d/1H48CuXQ32gHVkXeJ6oxbCvaSsabScrbw/view?usp=sharing' },
-  { name: 'Benefit Booklet', pdfUrl: 'https://drive.google.com/file/d/13aMjaInyR7KNKR0icao42NL2-O5Tl_Ft/view?usp=sharing' },
+  { name: 'Benefit Booklet', pdfUrl: 'https://drive.google.com/file/d/13UsCBflBEzgur-EA3OwMeitQXCA_uuux/view?usp=sharing' },
 ];
 
 
